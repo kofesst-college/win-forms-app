efaa83a3bb301fcd12e4f5e60c43dfc2a008387e
kof3st <xkof3st.mail@gmail.com>
2022-11-07 14:25:37 +0300

    Добавлен пользовательский элемент для ввода двузначных HEX чисел

---
+++ MultiSoftApp/Controls/HexTextBox.Designer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace MultiSoftApp.Controls
+{
+    partial class HexTextBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
---
+++ MultiSoftApp/Controls/HexTextBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace MultiSoftApp.Controls
+{
+    [ToolboxItem(true)]
+    public partial class HexTextBox : TextBox
+    {
+        private const int Minimum = 0x00;
+        private const int Maximum = 0xFF;
+
+        public int Value { get; private set; }
+        
+        public HexTextBox()
+        {
+            InitializeComponent();
+            TextChanged += OnTextChanged;
+            Leave += OnLeave;
+        }
+
+        private void OnLeave(object sender, EventArgs e)
+        {
+            Text = Value.ToString("X2");
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            Text = Text.ToUpper();
+            SelectionStart = Text.Length;
+            SelectionLength = 0;
+
+            if (Text.Length == 0)
+            {
+                Value = 0;
+                return;
+            }
+
+            if (Text.Length > 2)
+            {
+                Text = Value.ToString("X2");
+                return;
+            }
+
+            if (!IsValidInput(Text, out var hex))
+            {
+                Text = "";
+                return;
+            }
+
+            Value = hex;
+        }
+
+        private static bool IsValidInput(string text, out int value)
+        {
+            var parseResult = int.TryParse(text, NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture, out value);
+            return value >= Minimum &&
+                   value <= Maximum &&
+                   parseResult;
+        }
+    }
+}
